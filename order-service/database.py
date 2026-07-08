3e7039690b2012837c145a76fa4032357a5454d1
Saurabh Gupta <saurabhguptas3142@gmail.com>
2026-07-08 11:40:08 +0530

    docker file updated

--- order-service/database.py
+++ order-service/database.py
@@ -1,8 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./orders.db"
+os.makedirs("data", exist_ok=True)
+
+DATABASE_URL = "sqlite:///./data/orders.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
